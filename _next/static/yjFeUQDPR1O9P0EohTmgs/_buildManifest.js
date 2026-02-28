5a820355bf9a5f6ae44fe0ffb39874fa99c4c636
Vijay_xFaze <code.vijay.raj@gmail.com>
2026-02-28 21:54:57 +0530

    Updates

--- _next/static/yjFeUQDPR1O9P0EohTmgs/_buildManifest.js
+++ _next/static/yjFeUQDPR1O9P0EohTmgs/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/FS.io//_next/:path+",
+        "destination": "/FS.io/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
